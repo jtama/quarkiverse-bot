c6bb2390d853d9cd8807ead716955bf2cf5930c9
Jérôme TAMA <j.tama@groupeonepoint.com>
2021-04-28 11:13:14 +0200

    On va un peu plus loin

--- src/main/java/fr/kosmik/RespondComment.java
+++ src/main/java/fr/kosmik/RespondComment.java
@@ -2,14 +2,20 @@ package fr.kosmik;
 
 import io.quarkiverse.githubapp.event.Issue;
 import io.quarkiverse.githubapp.event.IssueComment;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.kohsuke.github.GHContent;
 import org.kohsuke.github.GHEventPayload;
 import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHPullRequest;
 import org.kohsuke.github.GHRef;
 import org.kohsuke.github.GHRepository;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class RespondComment {
 
@@ -38,9 +44,11 @@ public class RespondComment {
                     addComment(issueComment.getIssue(), "Of course but from  which branch ?");
                     return;
                 }
-                release(issueComment.getRepository(), commandAndArgs[2]);
+                release(issueComment.getRepository(), commandAndArgs[2], issueComment.getIssue().getNumber());
+                addComment(issueComment.getIssue(), "Pull request created");
+                return;
             default:
-                addComment(issueComment.getIssue(),"💀");
+                addComment(issueComment.getIssue(), "💀");
         }
     }
 
@@ -48,15 +56,40 @@ public class RespondComment {
         issue.comment(comment);
     }
 
-    private void release(GHRepository repository, String source) throws IOException {
+    private void release(GHRepository repository, String source, int issue) throws IOException {
         String sha = repository.getRef("heads/" + source).getObject().getSha();
-        String branchName = "release/1.2";
-        GHRef target = repository.createRef(branchName, sha);
-        repository.createContent().content(branchName).message(branchName).path(branchName).branch(branchName).commit();
-        repository.createPullRequest("Release 1.2", branchName, source, getPRDesc(repository), true);
+        String targetVersion = getTargetVersion(repository, source);
+        String branchName = "release/" + targetVersion;
+        GHRef ref = repository.createRef("refs/heads/" + branchName, sha);
+        amendProjectDescription(repository, targetVersion, branchName, ref);
+        repository.createPullRequest("Release " + targetVersion, branchName, source, getPRDesc(repository, targetVersion, issue), true);
     }
 
-    private String getPRDesc(GHRepository repo){
-        return String.format("## Release %s %s", repo.getName(), "1.2");
+    private void amendProjectDescription(GHRepository repository, String targetVersion, String branchName, GHRef ref) throws IOException {
+        String path = ".github/project.yml";
+        GHContent fileContent = repository.getFileContent(path, ref.getRef());
+        try (InputStream is = fileContent.read()) {
+            String content = IOUtils.toString(is, StandardCharsets.UTF_8);
+            content = content.replaceAll("(?m)(^\\s.current-version:\\s)(.*)", "*1"+targetVersion);
+            content = content.replaceAll("(?m)(^\\s.next-version:\\s)(.*)", "$1🚀");
+            repository.createContent().content(content).message("## Preparing release " + targetVersion).path(path).branch(branchName).sha(fileContent.getSha()).commit();
+        }
+    }
+
+    private String getTargetVersion(GHRepository repository, String source) throws IOException {
+        GHContent fileContent = repository.getFileContent(".github/project.yml", repository.getRef("heads/" + source).getRef());
+        try (InputStream is = fileContent.read()) {
+            String content = IOUtils.toString(is, StandardCharsets.UTF_8);
+            return StringUtils.substringBefore(Arrays.stream(content.split(System.lineSeparator()))
+                            .filter(line -> line.stripLeading().startsWith("next-version"))
+                            .findFirst()
+                            .map(line -> StringUtils.substringAfter(line, "next-version: "))
+                            .orElseThrow(() -> new IllegalArgumentException("Hoho, what is your target ???"))
+                    , "-SNAPSHOT");
+        }
+    }
+
+    private String getPRDesc(GHRepository repo, String targetVersion, int issue) {
+        return String.format("## Release %s %s\r\nCloses #%s", repo.getName(), targetVersion, issue);
     }
 }
